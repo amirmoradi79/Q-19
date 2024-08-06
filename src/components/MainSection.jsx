a67f6d635b6fd1421b647296b69ee1cadfb8d824
amir moradi <amir.moradi.work1379@gmail.com>
2024-08-06 18:48:46 +0330

    first commit

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,20 @@
+export default function MainSection() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-y-4 mt-8">
+        <p className="font-semibold text-3xl mb-4">My Todo List</p>
+        <div className="flex flex-col gap-y-3">
+          <input
+            type="text"
+            placeholder="what to do..."
+            className="border-[1px] border-black p-2 rounded-lg outline-none"
+          />
+          <button
+            type="button"
+            className="bg-gray-300 p-2 rounded-2xl"
+          ></button>
+        </div>
+      </div>
+    </>
+  );
+}
